bfb0b75f033f4a913b43478cbef38f77c7bb18e6
DELL <DELL@DESKTOP-109PRQG>
2018-02-12 09:52:14 -0700

    Agregar archivos de proyecto.

---
+++ JuegoArcade/JuegoArcade.cpp
@@ -0,0 +1,20 @@
+// JuegoArcade.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#define GLEW_STATIC
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
